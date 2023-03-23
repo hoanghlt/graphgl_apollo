2760cdc1f6a2b7c8b7bb888963cd96cf6ad091a4
Võ Tấn Hoàng <4hoangphuson15031995@gmail.com>
2023-03-23 17:00:58 +0700

    Update resolvers.js
    Change get data follow db.js, config connect mongodb

--- server/resolvers/resolvers.js
+++ server/resolvers/resolvers.js
@@ -1,23 +1,29 @@
-const {books,authors} = require('../data/static')
-
 const resolvers = {
     Query: {
-        books: () => books,
-        book: (parent, args) => books.find(book => book.id.toString() === args.id),
-        authors: () => authors,
-        author: (parent, args) => authors.find(author => author.id.toString() === args.id)
+        books: async (parent, args, {mongoDataMethods}) => 
+                await mongoDataMethods.getAllBooks(),
+        book: async (parent, {id}, {mongoDataMethods}) => 
+                await mongoDataMethods.getBookByid(id),
+        authors: async (parent, args, {mongoDataMethods}) => 
+                await mongoDataMethods.getAllAuthors(),
+        author: async (parent, {id}, {mongoDataMethods}) => 
+                await mongoDataMethods.getAuthorByid(id)
     },
     Book: {
-        author: (parent, args) => authors.find(author => author.id == parent.authorId)
+        author: async ({authorId}, args, {mongoDataMethods}) => 
+                await mongoDataMethods.getAuthorByid(authorId)
     },
     Author: {
-        books: (parent, args) => books.filter(book => book.authorId == parent.id)
+        books: async ({id}, args, {mongoDataMethods}) => 
+                await mongoDataMethods.getAllBooks({ authorId: id })
     },
 
     //MUTATION
     Mutation: {
-        createAuthor: (parent, args) => args,
-        createBook: (parent, args) => args
+        createAuthor: async (parent, args, {mongoDataMethods}) => 
+                mongoDataMethods.createAuthor(args),
+        createBook: async (parent, args, {mongoDataMethods}) => 
+                mongoDataMethods.createBook(args)
     }
 }
 
